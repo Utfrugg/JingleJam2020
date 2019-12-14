543a25bee34bb710f29d5c4c72012a842daa5e87
Utfrugg <170462@ade-nhtv.nl>
2019-12-14 12:29:18 +0100

    Upload Project
    Upload the base project with the .gitignore file working now

---
+++ JingleJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        //bleh
+
+        Vector2 movement = new Vector2(hor, ver);
+
+        rigidbody.AddForce(movement * speed);
+    }
+}
