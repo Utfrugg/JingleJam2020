d71caaa8e42b386ab97f819e578d76afe9594a56
Giraffaell <160038@ade-buas.nl>
2019-12-14 14:08:32 +0100

    Added follow camera

---
+++ JingleJam/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    //public float smoothspeed = o.125f
+    public Vector3 offset;
+
+    void LateUpdate ()
+    {
+        transform.position = target.position + offset;
+    }
+
+}
